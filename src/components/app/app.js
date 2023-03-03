8121a1a417c9ee0d0aa0266d0028c61b4283dbee
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-03 23:13:28 +0200

    main

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form'
 import './app.css';
 
 function App(){
+
+    const data = [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Smith M.', salary: 1000, increase: true, id: 2},
+        {name: 'Joe S.', salary: 1200, increase: false, id: 3}
+    ]
+
     return (
         <div className="app">
             <AppInfo></AppInfo>
@@ -14,7 +21,7 @@ function App(){
                 <SearchPanel></SearchPanel>
                 <AppFilter></AppFilter>                
             </div>
-            <EmployeesList></EmployeesList>
+            <EmployeesList data={data}></EmployeesList>
             <EmployeesAddForm></EmployeesAddForm>
         </div>
     )

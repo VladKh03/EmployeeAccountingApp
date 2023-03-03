ba47526bd380f22b53f1cdc8a10c6866bc76c64a
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-03 16:25:26 +0200

    main

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-
-const text = 'Hello';
-
-const elem = (
-	<div>	
-		<h2>{text}</h2>
-		<h2>{['2',2+2,'54']}</h2>
-		<input type="text" />
-		<button>Click</button>		
-	</div>
-	
-);
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
 );
 
 

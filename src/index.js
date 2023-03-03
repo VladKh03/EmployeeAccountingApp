423c6b9a29a0563da0e7e875c17b1ca112e400dc
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-03 21:06:08 +0200

    main

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './components/app/app';
 
 import './index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );

d08da6f100bf2b8ea8f504d30b66e8882860ca44
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-03 21:33:26 +0200

    main

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,21 @@
+import SearchPanel from '../search-panel/search-panel';
+import AppInfo from '../app-info/app-info';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form'
+
 import './app.css';
 
 function App(){
     return (
         <div className="app">
-            
+            <AppInfo></AppInfo>
+            <div className="search-panel">
+                <SearchPanel></SearchPanel>
+                <AppFilter></AppFilter>                
+            </div>
+            <EmployeesList></EmployeesList>
+            <EmployeesAddForm></EmployeesAddForm>
         </div>
     )
 }

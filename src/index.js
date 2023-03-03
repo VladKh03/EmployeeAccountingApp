3f738786f3ad016c81cba860f5b580bddd43145a
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-03 15:59:42 +0200

    main

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 
+const text = 'Hello';
+
+const elem = (
+	<div>	
+		<h2>{text}</h2>
+		<h2>{['2',2+2,'54']}</h2>
+		<input type="text" />
+		<button>Click</button>		
+	</div>
+	
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
-// test 2
+
 
 
